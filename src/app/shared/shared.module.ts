c8fd1b1b78fdadc94ad13469ab14ad1275e8610f
wobin1 <nathanielmusa3@gmail.com>
2024-09-19 07:16:04 +0100

    fixed import issue

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,3 @@
-import { SearchUploadHeaderComponent } from './../../../../../angular-projects/civet-ui/src/app/shared/search-upload-header/search-upload-header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './component/menu/menu.component';
@@ -7,6 +6,7 @@ import { OurserviceComponent } from './component/ourservice/ourservice.component
 import { FooterComponent } from './component/footer/footer.component';
 import { ImageCardComponent } from './component/image-card/image-card.component';
 import { AboutHeroComponent } from './component/about-hero/about-hero.component';
+import { SearchUploadHeaderComponent } from './component/search-upload-header/search-upload-header.component';
 
 
 
